8ab252cca4c126ffbe43cc22746ffc6346c9ab31
igor <igorkfz31@gmail.com>
2026-02-20 13:27:36 -0300

    project created

---
+++ src/main/java/com/workshop/maxsales/MaxsalesApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.maxsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxsalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxsalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workshop/maxsales/MaxsalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.maxsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxsalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
